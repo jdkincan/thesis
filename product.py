d48743db39e2613018500adeaee089b0594b12eb
jdkincan <jdkincan@uark.edu>
2021-12-27 22:40:05 -0600

    testing git PAT storage in terminal.

--- product.py
+++ product.py
@@ -5,5 +5,6 @@ import os
 from dotenv import load_dotenv
 from alpha_vantage.cryptocurrencies import CryptoCurrencies
 from alpha_vantage.techindicators import TechIndicators
+import pandas as pd
 
 
